4e36fc83ac0c8da0e4fbc1607e9f7b325992089d
Pawel Rogoza <pawel.rogoza@gmail.com>
2023-02-14 01:54:15 +0100

    brightness and volume control for openbox

--- .local/bin/chg_brightness.sh
+++ .local/bin/chg_brightness.sh
@@ -3,9 +3,22 @@
 MESSAGE="Brightness set to "
 if [ "$1" == "up" ] ; then
     MESSAGE="Brightness increased to "
+    SIGN="+"
+    THRESHOLD=19
 elif [ "$1" == "down" ] ; then 
     MESSAGE="Brightness decreased to "
+    SIGN="-"
+    THRESHOLD=21
 fi
+
+CURR_LEVEL=`xbacklight -get`
+if [ $CURR_LEVEL -lt $THRESHOLD ] ; then
+  STEP=5
+else
+  STEP=10
+fi
+
+xbacklight ${SIGN}${STEP}
 
 NEW_LEVEL=`xbacklight -get`
 
--- .local/bin/chg_volume.sh
+++ .local/bin/chg_volume.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "mute" ] ; then
+    pamixer -t
     if [ `pamixer --get-mute` == "true" ] ; then
         MUTE_STATUS="on"
     else
@@ -13,10 +14,13 @@ fi
 MESSAGE="Volume set to "
 if [ "$1" == "up" ] ; then
     MESSAGE="Volume increased to "
+    pamixer --increase 5 
 elif [ "$1" == "down" ] ; then 
     MESSAGE="Volume decreased to "
+    pamixer --decrease 5
 fi
 
 NEW_LEVEL=`pamixer --get-volume`
 
 notify-send "$MESSAGE" -u low -h int:value:$NEW_LEVEL -h string:synchronous:volume
+
